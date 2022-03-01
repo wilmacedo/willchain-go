c50d81e137cf5583323c425b44245a5085c77e4a
Wil Macedo <wil.macedo.sa@gmail.com>
2022-02-28 21:09:35 -0300

    add internal storage and iterators

--- models/blockchain.go
+++ models/blockchain.go
@@ -1,20 +1,89 @@
 package models
 
+import (
+	"fmt"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+const (
+	dbPath = "./tmp/blocks"
+)
+
 type Blockchain struct {
-	Blocks []*Block
+	LastHash []byte
+	Database *leveldb.DB
 }
 
-func (chain *Blockchain) AddBlock(data string) {
-	previousBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, previousBlock.Hash)
-
-	chain.Blocks = append(chain.Blocks, new)
+type Iterator struct {
+	CurrentHash []byte
+	Database    *leveldb.DB
 }
 
 func InitBlockchain() *Blockchain {
+	var lastHash []byte
+
+	db, err := leveldb.OpenFile(dbPath, nil)
+	Handle(err)
+
+	if _, err := db.Get([]byte("lh"), nil); err == leveldb.ErrNotFound {
+		fmt.Println("No existing blockchain found")
+		genesis := Genesis()
+		fmt.Println("Genesis proved")
+
+		err := db.Put(genesis.Hash, genesis.Serialize(), nil)
+		Handle(err)
+
+		err = db.Put([]byte("lh"), genesis.Hash, nil)
+		Handle(err)
+
+		lastHash = genesis.Hash
+	} else {
+		lastHash, err = db.Get([]byte("lh"), nil)
+		Handle(err)
+	}
+
 	chain := &Blockchain{
-		Blocks: []*Block{Genesis()},
+		LastHash: lastHash,
+		Database: db,
 	}
 
 	return chain
+}
+
+func (chain *Blockchain) AddBlock(data string) {
+	lastHash, err := chain.Database.Get([]byte("lh"), nil)
+	Handle(err)
+
+	newBlock := CreateBlock(data, lastHash)
+
+	err = chain.Database.Put(newBlock.Hash, newBlock.Serialize(), nil)
+	Handle(err)
+
+	err = chain.Database.Put([]byte("lh"), newBlock.Hash, nil)
+	Handle(err)
+
+	chain.LastHash = newBlock.Hash
+}
+
+func (chain *Blockchain) Iterator() *Iterator {
+	iter := &Iterator{
+		CurrentHash: chain.LastHash,
+		Database:    chain.Database,
+	}
+
+	return iter
+}
+
+func (iter *Iterator) Next() *Block {
+	var block *Block
+
+	encodedBlock, err := iter.Database.Get(iter.CurrentHash, nil)
+	Handle(err)
+
+	block = Deserialize(encodedBlock)
+
+	iter.CurrentHash = block.PreviousHash
+
+	return block
 }
